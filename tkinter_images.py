4982b0a72ef637b49e112456ec80d1cbe119a599
Soundarya S M <71707474+soundaryasm15@users.noreply.github.com>
2021-12-27 23:59:31 +0530

    Update tkinter_images.py

--- tkinter_images.py
+++ tkinter_images.py
@@ -12,6 +12,8 @@ my_img5=ImageTk.PhotoImage(Image.open("images/5.jpg"))
 
 image_list=[my_img1,my_img2,my_img3,my_img4,my_img5]
 
+status=Label(root,text="Image 1 of "+str(len(image_list)),bd=1,relief=SUNKEN,anchor=E)
+
 my_label=Label(image=my_img1)
 my_label.grid(row=0,column=0,columnspan=3)
 
@@ -32,7 +34,9 @@ def forward(image_number):
 	my_label.grid(row=0,column=0,columnspan=3)
 	button_backward.grid(row=1,column=0)
 	button_forward.grid(row=1,column=2)
-
+	#update status bar
+	status=Label(root,text="Image "+str(image_number) +" of "+str(len(image_list)),bd=1,relief=SUNKEN,anchor=E)
+	status.grid(row=2,column=0,columnspan=3,sticky=W+E)
 
 
 def backward(image_number):
@@ -52,6 +56,11 @@ def backward(image_number):
 	my_label.grid(row=0,column=0,columnspan=3)
 	button_backward.grid(row=1,column=0)
 	button_forward.grid(row=1,column=2)
+	#upadte status bar
+	status=Label(root,text="Image "+str(image_number) + " of "+str(len(image_list)),bd=1,relief=SUNKEN,anchor=E)
+	status.grid(row=2,column=0,columnspan=3,sticky=W+E)
+
+
 
 button_backward=Button(root,text="<<",command=backward,state=DISABLED)
 button_exit=Button(root,text="EXIT",command=root.quit)
@@ -63,7 +72,8 @@ button_quit.pack()'''
 
 button_backward.grid(row=1,column=0)
 button_exit.grid(row=1,column=1)
-button_forward.grid(row=1,column=2)
+button_forward.grid(row=1,column=2,pady=10)
+status.grid(row=2,column=0,columnspan=3,sticky=W+E)
 
 
 root.mainloop()
