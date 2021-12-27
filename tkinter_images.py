9afde2b475d46b5d38ef16f4943299e8910bd6af
Soundarya S M <71707474+soundaryasm15@users.noreply.github.com>
2021-12-27 23:39:05 +0530

    Add files via upload

---
+++ tkinter_images.py
@@ -0,0 +1,69 @@
+from tkinter import *
+from PIL import ImageTk,Image
+root=Tk()
+root.title("My window")
+root.iconbitmap("C:/Users/user/Desktop/college/5th sem/tkinter/home.ico")
+
+my_img1=ImageTk.PhotoImage(Image.open("images/1.jpg"))
+my_img2=ImageTk.PhotoImage(Image.open("images/2.jpg"))
+my_img3=ImageTk.PhotoImage(Image.open("images/3.jpg"))
+my_img4=ImageTk.PhotoImage(Image.open("images/4.jpg"))
+my_img5=ImageTk.PhotoImage(Image.open("images/5.jpg"))
+
+image_list=[my_img1,my_img2,my_img3,my_img4,my_img5]
+
+my_label=Label(image=my_img1)
+my_label.grid(row=0,column=0,columnspan=3)
+
+def forward(image_number):
+	global my_label
+	global button_forward
+	global button_backward
+
+	my_label.grid_forget()
+	
+	my_label=Label(image=image_list[image_number-1])
+	button_forward=Button(root,text=">>",command=lambda: forward(image_number+1))
+	button_backward=Button(root,text="<<",command=lambda: backward(image_number-1))
+
+	if image_number==5:
+		button_forward=Button(root,text=">>",state=DISABLED)
+
+	my_label.grid(row=0,column=0,columnspan=3)
+	button_backward.grid(row=1,column=0)
+	button_forward.grid(row=1,column=2)
+
+
+
+def backward(image_number):
+	global my_label
+	global button_forward
+	global button_backward
+	
+	my_label.grid_forget()
+	my_label=Label(image=image_list[image_number-1])
+	button_forward=Button(root,text=">>",command=lambda: forward(image_number+1))
+	button_backward=Button(root,text="<<",command=lambda: backward(image_number-1))
+
+
+	if image_number==1:
+		button_backward=Button(root,text="<<",state=DISABLED)
+
+	my_label.grid(row=0,column=0,columnspan=3)
+	button_backward.grid(row=1,column=0)
+	button_forward.grid(row=1,column=2)
+
+button_backward=Button(root,text="<<",command=backward,state=DISABLED)
+button_exit=Button(root,text="EXIT",command=root.quit)
+button_forward=Button(root,text=">>",command=lambda: forward(2))
+
+
+'''button_quit=Button(root,text="Exit!!!",command=root.quit)
+button_quit.pack()'''
+
+button_backward.grid(row=1,column=0)
+button_exit.grid(row=1,column=1)
+button_forward.grid(row=1,column=2)
+
+
+root.mainloop()
